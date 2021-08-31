702026913ffcbd9d4abf78a2da8d3001e0ae9a03
hafsajan <79843377+hafsajan@users.noreply.github.com>
2021-08-31 18:48:41 +0530

    Add files via upload

---
+++ Bird.js
@@ -0,0 +1,26 @@
+class Bird extends BaseClass {
+  constructor(x,y){
+    super(x,y,50,50);
+    this.image = loadImage("sprites/bird.png");
+    this.smokeimage=loadImage("sprites/smoke.png");
+    this.traj=[]
+
+  }
+
+  display() {
+    
+    //this.body.position.x = mouseX;
+    //this.body.position.y = mouseY;
+    super.display();
+    if(this.body.velocity.x>0 && this.body.position.x>220){
+      var position=[this.body.position.x,this.body.position.y];
+      this.traj.push(position);  
+    }
+    
+    for(var i=0;i<this.traj.length;i++){
+
+    image(this.smokeimage,this.traj[i][0],this.traj[i][1],5,5);
+
+    };
+  }
+}
